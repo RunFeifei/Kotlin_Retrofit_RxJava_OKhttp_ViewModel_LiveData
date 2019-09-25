d69935507f182366d24debb559beed72bf5175e8
PengFei <feifei.feixia@163.com>
2019-09-25 23:06:45 +0800

    addLoading

---
+++ app/src/main/java/com/uestc/medicine/util/LoadingDialog.java
@@ -0,0 +1,36 @@
+package com.uestc.medicine.util;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.uestc.medicine.R;
+
+public class LoadingDialog extends Dialog {
+
+    public LoadingDialog(Context context) {
+        super(context);
+    }
+
+    public LoadingDialog(Context context, int theme) {
+        super(context, theme);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init(getContext());
+    }
+
+    private void init(Context context) {
+        setCancelable(true);
+        setCanceledOnTouchOutside(false);
+        setContentView(R.layout.loading);
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        getWindow().setAttributes(params);
+    }
+}
